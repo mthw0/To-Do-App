93afdd4d92b65ec9475a7a945007170db086e70c
Matej Mrenica <matejm98mthw@gmail.com>
2022-07-22 20:46:03 +0200

    e-mail notif added and tested

--- app/Notifications/EmailNotification.php
+++ app/Notifications/EmailNotification.php
@@ -31,7 +31,7 @@ class EmailNotification extends Notification
      */
     public function via($notifiable)
     {
-        return [ 'database'];
+        return [ 'mail','database'];
     }
 
     /**
